8f7785b5a361288dd6a03c44fd8efd55b253fa53
dikshajn <dikshaajn@gmail.com>
2025-01-22 02:20:21 +0530

    Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -20,5 +20,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-
+# if __name__=="__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("logging started")
+#         raise CustomException(e,sys)
         
